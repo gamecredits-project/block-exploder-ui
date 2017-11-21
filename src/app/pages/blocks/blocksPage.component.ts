1b5d3757d6040ce18183db40cf6ee9ee5e46e392
marko_ristic <marko.risticcc@gmail.com>
2017-11-21 14:24:52 +0100

    White blocks - desktop

--- src/app/pages/blocks/blocksPage.component.ts
+++ src/app/pages/blocks/blocksPage.component.ts
@@ -1,19 +1,20 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
-import { BlocksPageService } from "./blocksPage.service";
-import { BlockSocketService } from "app/pages/socket/socket.service";
-import {Subscription} from "rxjs";
+import {BlocksPageService} from './blocksPage.service';
+import {BlockSocketService} from '../socket/socket.service';
+// import {Subscription} from 'rxjs';
+import {Globals} from '../globals';
 
-@Component ({
- 	selector: 'exploder-blocks',
- 	templateUrl: 'blocksPage.component.html',
-  providers: [BlockSocketService]
+@Component({
+  selector   : 'exploder-blocks',
+  templateUrl: 'blocksPage.component.html',
+  providers  : [BlockSocketService]
 })
 
 export class BlocksPageComponent implements OnInit, OnDestroy {
 
- 	blockArray: any = [];
- 	currentlyLoaded: number = 0;
- 	loadPerScroll: number = 30;
+  blockArray: any = [];
+  currentlyLoaded: number = 0;
+  loadPerScroll: number = 30;
 
   private socket: any;
   private block_response: any;
@@ -23,67 +24,66 @@ export class BlocksPageComponent implements OnInit, OnDestroy {
   private block_data: any;
 
 
- 	constructor(private blocksPageService: BlocksPageService, private blockSocketService: BlockSocketService) {
+  constructor(private blocksPageService: BlocksPageService, private blockSocketService: BlockSocketService, public globals: Globals) {
     this.blocks = [];
   }
 
- 	ngOnInit() {
+  ngOnInit() {
     this.addBlocks();
 
     this.socket = this.blockSocketService.initConnection();
 
     this.getBlockInitMessage();
     this.getSocketBlock();
- 	}
+  }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.socket.unsubscribe();
   }
 
- 	addBlocks(){
- 		this.blocksPageService.getBlocks(this.loadPerScroll, this.currentlyLoaded).subscribe( (resp) => {
-			this.blockArray = this.blockArray.concat(resp);
-		});
-		this.currentlyLoaded += this.loadPerScroll;
- 	}
+  addBlocks() {
+    this.blocksPageService.getBlocks(this.loadPerScroll, this.currentlyLoaded).subscribe((resp) => {
+      this.blockArray = this.blockArray.concat(resp);
+    });
+    this.currentlyLoaded += this.loadPerScroll;
+  }
 
- 	onScroll() {
- 		this.addBlocks();
- 	}
+  onScroll() {
+    this.addBlocks();
+  }
 
   private getBlockInitMessage(): void {
-    this.socket = this.blockSocketService.getBlockConnection().subscribe((block_response) =>{
-      this.block_test = block_response
+    this.socket = this.blockSocketService.getBlockConnection().subscribe((block_response) => {
+      this.block_test = block_response;
     });
   }
 
   private getSocketBlock(): void {
-    this.socket = this.blockSocketService.getBlock().subscribe((block_data) =>{
+    this.socket = this.blockSocketService.getBlock().subscribe((block_data) => {
       this.blocks.push(block_data);
     });
   }
 
 
+  calulateMinutesFromNow(time: number) {
+    let now = new Date();
+    let minutesOld = Math.abs(Math.round((now.valueOf() / 1000 - time) / 60));
+    let message = '';
+    let days = Math.floor(minutesOld / (60 * 24));
+    let hours = Math.floor(( minutesOld - days * 60 * 24 ) / 60);
+    let minutes = minutesOld % 60;
 
- 	calulateMinutesFromNow( time: number) {
- 		let now = new Date();
- 		let minutesOld = Math.abs( Math.round( (now.valueOf() / 1000 - time) / 60 ) );
- 		let message = '';
- 		let days = Math.floor(minutesOld / (60 * 24) );
- 		let hours = Math.floor( ( minutesOld - days * 60 * 24 ) / 60 );
- 		let minutes = minutesOld % 60;
 
+    message += minutes + 'm ';
 
- 		message += minutes + 'm ';
+    if (hours || days) {
+      message += hours + 'h ';
+    }
 
- 		if(hours || days) {
- 			message += hours + 'h ';
- 		}
+    if (days) {
+      message += days + 'd ';
+    }
 
- 		if(days) {
- 			message += days + 'd ';
- 		}
-
- 		return message;
- 	}
- }
+    return message;
+  }
+}
