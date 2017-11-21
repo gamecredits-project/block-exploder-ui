0b59d49392f0a487e61b3b0b08ab21dc2e97e860
marko_ristic <marko.risticcc@gmail.com>
2017-11-21 13:48:00 +0100

    Transaction white theme

--- src/app/pages/transactions/transactionsPage.component.ts
+++ src/app/pages/transactions/transactionsPage.component.ts
@@ -1,35 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { TransactionsPageService } from "./transactionsPage.service";
-import { ActivatedRoute, Params }   from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {TransactionsPageService} from './transactionsPage.service';
+import {ActivatedRoute, Params} from '@angular/router';
+import { Globals } from '../globals';
 
 import 'rxjs/add/operator/switchMap';
 
- @Component ({
- 	selector: 'exploder-transactions',
- 	templateUrl: 'transactionsPage.component.html'
- })
- export class TransactionsPageComponent implements OnInit {
+@Component({
+  selector   : 'exploder-transactions',
+  templateUrl: 'transactionsPage.component.html'
+})
+export class TransactionsPageComponent implements OnInit {
 
- 	transaction: any;
- 	confirmations: any;
- 	currentLocation: string ="www.blockexplorer.gamecredits.com";
+  transaction: any;
+  confirmations: any;
+  currentLocation: string = 'www.blockexplorer.gamecredits.com';
 
- 	constructor(private transactionsPageService: TransactionsPageService,
- 		private route: ActivatedRoute
-		) {}
+  constructor(private transactionsPageService: TransactionsPageService,
+              private route: ActivatedRoute, public globals: Globals) {
+  }
 
- 	ngOnInit() {
+  ngOnInit() {
     this.currentLocation = window.location.href;
- 		this.route.params
- 			.switchMap((params: Params) => this.transactionsPageService.getTransaction('' + params['txid']))
- 			.subscribe( resp => {
- 				this.transaction = resp;
+    this.route.params
+      .switchMap((params: Params) => this.transactionsPageService.getTransaction('' + params['txid']))
+      .subscribe(resp => {
+        this.transaction = resp;
 
-				this.transactionsPageService.getBlockConfirmations(this.transaction.blockhash)
-	 			.subscribe( resp => {
-	 				this.confirmations = resp.confirmations;
-	 			});
+        this.transactionsPageService.getBlockConfirmations(this.transaction.blockhash)
+          .subscribe(resp => {
+            this.confirmations = resp.confirmations;
+          });
 
- 			});
- 	}
- }
+      });
+  }
+}
