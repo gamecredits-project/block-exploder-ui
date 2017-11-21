0245b4fdf0da29077fca636ddec29e5ebb2f4191
marko_ristic <marko.risticcc@gmail.com>
2017-11-21 18:23:12 +0100

    Single Block page

--- src/app/pages/block/blockPage.component.ts
+++ src/app/pages/block/blockPage.component.ts
@@ -1,39 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { BlockPageService } from "./blockPage.service";
-import { ActivatedRoute, Params }   from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {BlockPageService} from './blockPage.service';
+import {ActivatedRoute, Params} from '@angular/router';
+import {Globals} from '../globals';
 
 import 'rxjs/add/operator/switchMap';
 
- @Component ({
- 	selector: 'exploder-block',
- 	templateUrl: 'blockPage.component.html'
- })
- export class BlockPageComponent implements OnInit {
+@Component({
+  selector   : 'exploder-block',
+  templateUrl: 'blockPage.component.html',
+  styleUrls  : ['./block.style.css'],
+})
+export class BlockPageComponent implements OnInit {
 
- 	block: any;
- 	confirmations: any;
- 	transactions: any;
+  block: any;
+  confirmations: any;
+  transactions: any;
 
- 	constructor(private blockPageService: BlockPageService, 
- 		private route: ActivatedRoute
-		) {}
+  constructor(private blockPageService: BlockPageService,
+              private route: ActivatedRoute,
+              public globals: Globals) {
+  }
 
- 	ngOnInit() {
- 		this.route.params
- 			.switchMap((params: Params) => this.blockPageService.getBlock('' + params['hash']))
- 			.subscribe( resp => {
- 				this.block = resp;
- 			});
+  ngOnInit() {
+    this.route.params
+      .switchMap((params: Params) => this.blockPageService.getBlock('' + params['hash']))
+      .subscribe(resp => {
+        this.block = resp;
+      });
 
- 		this.route.params
- 			.switchMap((params: Params) => this.blockPageService.getBlockConfirmations('' + params['hash']))
- 			.subscribe( resp => {
- 				this.confirmations = resp;
- 			});
- 		this.route.params
- 			.switchMap((params: Params) => this.blockPageService.getBlockTransactions('' + params['hash']))
- 			.subscribe( resp => {
- 				this.transactions = resp;
- 			});
- 	}
- }
+    this.route.params
+      .switchMap((params: Params) => this.blockPageService.getBlockConfirmations('' + params['hash']))
+      .subscribe(resp => {
+        this.confirmations = resp;
+      });
+    this.route.params
+      .switchMap((params: Params) => this.blockPageService.getBlockTransactions('' + params['hash']))
+      .subscribe(resp => {
+        this.transactions = resp;
+      });
+  }
+}
